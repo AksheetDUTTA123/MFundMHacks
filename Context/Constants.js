e0faac9fac9eec79de0aca0d3e463582df0eed44
angeete <angeete@umich.edu>
2024-09-29 11:44:46 -0400

    asd

---
+++ Context/Constants.js
@@ -0,0 +1,3 @@
+import crowdFunding from './CrowdFunding.json';
+export const CROWDFUNDING_ADDRESS = '0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199'; // Update this address
+export const CROWDFUNDING_ABI = crowdFunding.abi;
